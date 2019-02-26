c06e3f08a1f1495de14ef838bb80054484f84c70
ellie turnock <ebturnock@gmail.com>
2019-02-26 16:08:38 +0000

    added bit route, controller and model

---
+++ routes/bit.route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+
+// Require the controllers
+const bit_controller = require('../controllers/bit.controller');
+
+//a simple test url to check that all of our files are communicating correctly
+router.get('/test', bit_controller.test);
+
+module.exports = router;
+const urlEncodedParser = bodyParser.urlencoded({extended: false});
+router.post('/create', urlEncodedParser, bit_controller.bit_create);
+router.get('/:id', bit_controller.bit_details);
+router.put('/:id/update', urlEncodedParser, bit_controller.bit_update);
+router.delete('/:id/delete', bit_controller.bit_delete);
