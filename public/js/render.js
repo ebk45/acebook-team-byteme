d634101bc61746eea873d8a8fe0ca9432ee6a50b
jesslns <jesslns@yahoo.com.hk>
2019-03-01 14:08:25 +0000

    reformat date display for bit timestamp

--- public/js/render.js
+++ public/js/render.js
@@ -9,10 +9,13 @@ const getBits = function() {
 const displayBits = data => {
   bitsList.innerHTML = data
     .map((bit, index) => {
+      date = new Date(bit.createdAt).toUTCString();
+      displayDate = date.split(' ').slice(0, 5).join(' ');
+
       return `
           <div class="container"><div class="card" id="bit-${index}">
             <p>${bit.post}</p>
-            <p>${bit.createdAt}</p>
+            <p>${displayDate}</p>
           </div></div>`;
     })
     .join("");
