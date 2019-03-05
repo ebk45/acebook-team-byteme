80afef52e0bf45f9b63d439a69b8a5ac8dd92ef6
ellie turnock <ebturnock@gmail.com>
2019-03-05 15:58:18 +0000

    added cors to backend

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const user = require("./routes/user.route");
@@ -15,11 +16,12 @@ mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+app.use(cors());
 app.use("/users", user);
 app.use("/bits", bit);
 app.use(express.static(__dirname + "/public"));
 
-// configure xpress to use body-parser as a middleware
+// configure xpress to use body-parser and cors as a middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
